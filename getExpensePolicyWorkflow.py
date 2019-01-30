cb07f01f2c0c92162af256931d968bca7b6142ac
Pablo Mujica <pmujica@overactiveinc.com>
2019-01-30 19:02:11 -0300

    clenaing for pep8 and ingoring for editors config folders

--- getExpensePolicyWorkflow.py
+++ getExpensePolicyWorkflow.py
@@ -1,5 +1,4 @@
 import Sdkrindegastos
-import json
 
 token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiNjEiLCJjb21wYW55X2lkIjoiMTA5IiwicmFuZG9tIjoicmFuZEFQSTVhYTgzZTI4OWY0NWQzLjUwNDczNzY4In0.arDxsQlJlssjrUoh2Dx_72-wDm_RSYXDSO7s0pkFtIc"
 r = Sdkrindegastos.Rindegastos(token)
